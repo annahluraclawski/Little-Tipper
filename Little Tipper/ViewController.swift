f5c450156f88ead5898a972194a8d0234c26925f
Annahlu Raclawski <annahlu47@gmail.com>
2023-08-30 09:47:09 -0500

    Initial Commit

---
+++ Little Tipper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Little Tipper
+//
+//  Created by ANNAHLU RACLAWSKI on 8/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
