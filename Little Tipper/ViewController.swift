435e0a91106c0f9787819606f85a50b0c8c9d925
Annahlu Raclawski <annahlu47@gmail.com>
2023-09-11 10:17:36 -0500

    done

--- Little Tipper/ViewController.swift
+++ Little Tipper/ViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate{
 
+    @IBOutlet weak var imageOutlet: UIImageView!
+    
+    @IBOutlet weak var amountOutlet: UILabel!
+    
+    @IBOutlet weak var billAmountOutlet: UITextField!
+    
+    @IBOutlet weak var tipAmountOutlet: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        billAmountOutlet.delegate = self
+        tipAmountOutlet.delegate = self
     }
 
-
+    @IBAction func calculateAction(_ sender: UIButton) {
+        let amount = billAmountOutlet.text!
+        let tip = tipAmountOutlet.text
+        let percentage = Double(tip!)! / 100
+        let total = Double(amount)! * percentage
+        amountOutlet.text = "\(total)"
+        imageOutlet.image = UIImage(named: "images")
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        billAmountOutlet.resignFirstResponder()
+        tipAmountOutlet.resignFirstResponder()
+        
+        return true
+    }
+    
 }
 
